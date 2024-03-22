031e23ba76c0a4da075de5bed968eeb71f51a64c
Scheila Stihler <sstihler@gmail.com>
2024-03-22 19:39:15 -0300

    Resolução do exercício 07

---
+++ src/main/java/br/com/senai/fullstack/senaifullstackeducationsemana08/controller/CursoController.java
@@ -0,0 +1,28 @@
+package br.com.senai.fullstack.senaifullstackeducationsemana08.controller;
+
+import br.com.senai.fullstack.senaifullstackeducationsemana08.model.CursoModel;
+import br.com.senai.fullstack.senaifullstackeducationsemana08.service.CursoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("cursos")
+public class CursoController {
+
+  private final CursoService cursoService;
+
+  public CursoController(CursoService cursoService) {
+    this.cursoService = cursoService;
+  }
+
+  @GetMapping
+  public List<CursoModel> get() {
+    return cursoService.buscarTodos();
+  }
+
+  @PostMapping
+  public CursoModel post(@RequestBody CursoModel curso) throws Exception {
+    return cursoService.salvar(curso);
+  }
+}
